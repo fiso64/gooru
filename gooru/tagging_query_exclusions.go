3d12eefe84ec553b6fbad9a9a7ac4e71edd9d45c
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-02 03:10:35 +0200

    fix(tags): use repository transaction wrapper

--- gooru/tagging_query_exclusions.go
+++ gooru/tagging_query_exclusions.go
@@ -1,10 +1,10 @@
 package gooru
 
 import (
-	"database/sql"
 	"fmt"
 	"strings"
 
+	"gooru.local/internal/database"
 	"gooru.local/internal/query"
 	"gooru.local/types"
 )
@@ -116,7 +116,7 @@ func (c *Client) mutateTagsByQueryExcluding(expression string, tags, excludedHas
 	return int(affected), tx.Commit()
 }
 
-func (c *Client) tagIDsForMutation(tx *sql.Tx, tags []string, create bool) ([]int64, error) {
+func (c *Client) tagIDsForMutation(tx *database.Tx, tags []string, create bool) ([]int64, error) {
 	parsed := make([]types.ParsedTag, len(tags))
 	for i, tag := range tags {
 		parsed[i] = query.ParseTag(tag)
