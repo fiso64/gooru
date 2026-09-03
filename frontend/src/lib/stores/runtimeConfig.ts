de8f152f57715e5de6eca7cf9315f92635d818bc
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-03 23:28:40 +0200

    fix(ui): track available thumbnail sizes

--- frontend/src/lib/stores/runtimeConfig.ts
+++ frontend/src/lib/stores/runtimeConfig.ts
@@ -1,13 +1,16 @@
 import { writable } from 'svelte/store';
 
 export const defaultGridSize = 180;
+export const defaultThumbnailSizes = [256, 512];
 
 export type RuntimeConfig = {
   loadFullMediaByDefault: boolean;
   gridSize: number;
+  thumbnailSizes: number[];
 };
 
 export const runtimeConfig = writable<RuntimeConfig>({
   loadFullMediaByDefault: false,
-  gridSize: defaultGridSize
+  gridSize: defaultGridSize,
+  thumbnailSizes: defaultThumbnailSizes
 });
