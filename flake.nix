95401e280d429f27765d28c0d2fdd3750eb4cd79
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-03 09:09:49 +0200

    fix(nix): refresh frontend dependency hash

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
             pname = "gooru-frontend";
             version = "0-unstable";
             src = ./frontend;
-            npmDepsHash = "sha256-k3d4Md1NQZfYO/dq1xzv5U8B5NRNhsAFPAv5BNkNVXM=";
+            npmDepsHash = "sha256-IwmulF0IrBA//GfuVagDZF4pZqFSOW9fHk0t0xmHWfU=";
             npmBuildScript = "build";
             installPhase = ''
               runHook preInstall
