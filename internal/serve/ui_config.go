3f2cadd0813802c4a21dc15c5d9ba435472fca4b
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-03 05:10:26 +0200

    feat(ui): expose font style preset

--- internal/serve/ui_config.go
+++ internal/serve/ui_config.go
@@ -4,12 +4,14 @@ import "net/http"
 
 type UIConfigResponse struct {
 	AccentColor            string `json:"accent_color,omitempty"`
+	FontStyle              string `json:"font_style"`
 	LoadFullMediaByDefault bool   `json:"load_full_media_by_default"`
 }
 
 func (s *Server) handleUIConfig(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, UIConfigResponse{
 		AccentColor:            s.cfg.UI.AccentColor,
+		FontStyle:              s.cfg.UI.FontStyle,
 		LoadFullMediaByDefault: s.cfg.Media.LoadFullByDefault,
 	})
 }
