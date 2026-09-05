7b9cf3caf391dbe3174f912b0a5dffb3157e4374
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-05 06:10:05 +0200

    refactor: resolve HTTP media through shared source abstraction

--- internal/serve/media_source.go
+++ internal/serve/media_source.go
@@ -1,13 +1,8 @@
 package serve
 
 import (
-	"errors"
-	"fmt"
 	"io"
-	"os"
 	"time"
-
-	"gooru.local/internal/encryptedfile"
 )
 
 type mediaReadSource interface {
@@ -24,47 +19,16 @@ type openedMediaSource struct {
 }
 
 func (m *MediaService) openMediaSource(path string) (*openedMediaSource, error) {
-	if m.cfg.Encryption.Enabled {
-		encrypted, err := encryptedfile.IsEncryptedFile(path)
-		if err != nil {
-			return nil, err
-		}
-		if encrypted {
-			file, err := encryptedfile.Open(path, m.cfg.Encryption.Key)
-			if err != nil {
-				return nil, err
-			}
-			return &openedMediaSource{
-				mediaReadSource: file,
-				size:            file.Size(),
-				modTime:         file.ModTime(),
-			}, nil
-		}
-		if IsManagedUploadPath(m.cfg.Uploads.Targets, path) {
-			return nil, fmt.Errorf("protected upload is not encrypted: %w", encryptedfile.ErrInvalidFormat)
-		}
+	if m.sourceResolverErr != nil {
+		return nil, m.sourceResolverErr
 	}
-
-	file, err := os.Open(path)
+	source, err := m.sourceResolver.Open(path)
 	if err != nil {
 		return nil, err
-	}
-	info, err := file.Stat()
-	if err != nil {
-		_ = file.Close()
-		return nil, err
-	}
-	if info.IsDir() {
-		_ = file.Close()
-		return nil, fmt.Errorf("media source is a directory")
-	}
-	if !info.Mode().IsRegular() {
-		_ = file.Close()
-		return nil, errors.New("media source is not a regular file")
 	}
 	return &openedMediaSource{
-		mediaReadSource: file,
-		size:            info.Size(),
-		modTime:         info.ModTime(),
+		mediaReadSource: source,
+		size:            source.Size(),
+		modTime:         source.ModTime(),
 	}, nil
 }
