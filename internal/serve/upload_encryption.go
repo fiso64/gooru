5a430fc8343524327fa5c8021623625200146fc0
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-05 07:31:41 +0200

    refactor: route uploads through managed writer

--- internal/serve/upload_encryption.go
+++ internal/serve/upload_encryption.go
@@ -1,26 +1,17 @@
 package serve
 
 import (
+	"errors"
 	"io"
 	"os"
 
-	"gooru.local/internal/encryptedfile"
+	"gooru.local/internal/managedfile"
 )
 
 func (s *Server) persistUploadedFile(dst *os.File, src io.Reader) (int64, error) {
-	if !s.cfg.Encryption.Enabled {
-		return copyUpload(dst, src, s.cfg.Uploads.MaxFileSizeBytes)
-	}
-	if s.cfg.Uploads.MaxFileSizeBytes <= 0 {
-		return encryptedfile.EncryptStream(dst, src, s.cfg.Encryption.Key)
-	}
-	limited := &io.LimitedReader{R: src, N: s.cfg.Uploads.MaxFileSizeBytes + 1}
-	size, err := encryptedfile.EncryptStream(dst, limited, s.cfg.Encryption.Key)
-	if err != nil {
-		return size, err
-	}
-	if size > s.cfg.Uploads.MaxFileSizeBytes {
+	size, err := s.managedFiles.Write(dst, src, s.cfg.Uploads.MaxFileSizeBytes)
+	if errors.Is(err, managedfile.ErrTooLarge) {
 		return size, errUploadTooLarge
 	}
-	return size, nil
+	return size, err
 }
