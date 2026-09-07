fe3fe2549bf1478af067ace844c80abe54b8f299
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-07 18:47:15 +0200

    refactor: avoid exposing storage query descriptors publicly

--- types/tag_facets.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// TagFacetExclusion describes one simple user-tag exclusion for aggregate
-// queries. KeyOnly matches every value in the namespace; otherwise Tag is an
-// exact key/value match.
-type TagFacetExclusion struct {
-	Tag     ParsedTag
-	KeyOnly bool
-}
