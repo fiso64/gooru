7cf7b0e8a9b1ee86ab12d7e6339c41507b0601b1
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-09 14:19:47 +0200

    fix(upload): drop disconnected added-order migration

--- internal/database/migrations/027_location_added_order.down.sql
+++
@@ -1,4 +0,0 @@
-DROP INDEX IF EXISTS idx_locations_added_order_desc_id_asc;
-DROP INDEX IF EXISTS idx_locations_added_order_id;
-DROP TRIGGER IF EXISTS set_location_added_order_on_insert;
-ALTER TABLE locations DROP COLUMN added_order;
