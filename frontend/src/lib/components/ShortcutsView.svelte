387f743c64f9aa0f69cfdde61322ff2c90b4d40c
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-14 13:44:58 +0200

    fix: document page navigation shortcuts (#686)

--- frontend/src/lib/components/ShortcutsView.svelte
+++ frontend/src/lib/components/ShortcutsView.svelte
@@ -13,6 +13,8 @@
       { keys: ['/'], description: 'Focus search' },
       { keys: ['f'], description: 'Search filenames' },
       { keys: ['b'], description: 'Save current search' },
+      { keys: ['⇧', 'PageUp'], description: 'Previous page' },
+      { keys: ['⇧', 'PageDown'], description: 'Next page' },
       { keys: ['?'], description: 'Show shortcuts' }
     ]
   } as const;
