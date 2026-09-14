88200e48dd16c3dc0f0b50bf797d84660b6962a1
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-14 17:34:39 +0200

    serve: stop scheduling upload-specific metadata finalizers

--- internal/serve/background_thumbnails.go
+++ internal/serve/background_thumbnails.go
@@ -126,7 +126,7 @@ func (s *Server) NewBackgroundRuntime(client *core.Client, workerID string) (Bac
 		ResourceClass: backgroundUploadResourceClass,
 		WorkerID:      workerID + "-upload",
 		Handlers: map[string]core.BackgroundTaskHandler{
-			backgroundUploadTaskKind:        s.backgroundUploadHandler(newBackgroundUploadFinalizingStore(client, client)),
+			backgroundUploadTaskKind:        s.backgroundUploadHandler(client),
 			backgroundUploadCleanupTaskKind: s.backgroundUploadCleanupHandlerV2(client),
 		},
 	})
