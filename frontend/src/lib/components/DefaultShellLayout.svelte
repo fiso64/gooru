e1134af13b2344dfff002401abdd8144ec6e367f
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-19 11:06:18 +0200

    fix: use endpoint candidates for viewer and main search completions (#875)

--- frontend/src/lib/components/DefaultShellLayout.svelte
+++ frontend/src/lib/components/DefaultShellLayout.svelte
@@ -29,7 +29,7 @@
           value={model.search}
           suggestions={model.suggestions}
           metaTags={model.metaTags}
-          tags={model.tags}
+          tags={[]}
           onDraftInput={actions.onSearchDraft}
           onCommit={actions.onSearchCommit}
         />
