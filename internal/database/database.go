7e1de3fd7eddee7f9843b8556b4e9474bc80576a
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-19 02:28:22 +0200

    refactor: reuse row batching for relink additions

--- internal/database/database.go
+++ internal/database/database.go
@@ -627,39 +627,21 @@ func (s *Store) ApplyRelinkAdditionsTx(q Querier, toAdd map[string]types.Locatio
 		return 0, nil
 	}
 
-	var locationsAdded int
-	const columns = 5 // content_hash, path, size_bytes, mod_time, extension
-	batchSize := maxVars / columns
-	var args []interface{}
-	var queryBuilder strings.Builder
-	// The tags_cache is now populated by triggers, so we don't insert it here.
-	queryBuilder.WriteString("INSERT OR IGNORE INTO locations (public_id, content_hash, path, size_bytes, mod_time, extension) VALUES ")
-
-	itemsInBatch := 0
+	additions := make([]types.LocationInfo, 0, len(toAdd))
 	for path, info := range toAdd {
-		if itemsInBatch > 0 {
-			queryBuilder.WriteString(", ")
-		}
-		queryBuilder.WriteString("('file_' || lower(hex(randomblob(16))), ?, ?, ?, ?, ?)")
-		args = append(args, info.Hash, path, info.Size, info.ModTime, info.Extension)
-		itemsInBatch++
-
-		if itemsInBatch >= batchSize {
-			res, err := q.Exec(queryBuilder.String(), args...)
-			if err != nil {
-				return 0, err
-			}
-			added, _ := res.RowsAffected()
-			locationsAdded += int(added)
-			itemsInBatch = 0
-			args = nil
-			queryBuilder.Reset()
-			queryBuilder.WriteString("INSERT OR IGNORE INTO locations (public_id, content_hash, path, size_bytes, mod_time, extension) VALUES ")
-		}
+		info.Path = path
+		additions = append(additions, info)
 	}
 
-	if itemsInBatch > 0 {
-		res, err := q.Exec(queryBuilder.String(), args...)
+	const columns = 5 // content_hash, path, size_bytes, mod_time, extension
+	const rowPlaceholders = "('file_' || lower(hex(randomblob(16))), ?, ?, ?, ?, ?)"
+	var locationsAdded int
+	for placeholders, args := range bindRowBatches(additions, rowPlaceholders, columns, func(args []any, info types.LocationInfo, _ int) {
+		args[0], args[1], args[2] = info.Hash, info.Path, info.Size
+		args[3], args[4] = info.ModTime, info.Extension
+	}) {
+		query := "INSERT OR IGNORE INTO locations (public_id, content_hash, path, size_bytes, mod_time, extension) VALUES " + placeholders
+		res, err := q.Exec(query, args...)
 		if err != nil {
 			return 0, err
 		}
