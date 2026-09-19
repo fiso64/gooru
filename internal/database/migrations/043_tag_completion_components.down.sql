e92245788d89026f8da9215bd12e5f6045598a2e
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-19 10:58:38 +0200

    db: provide tag completion index rollback (#875)

---
+++ internal/database/migrations/043_tag_completion_components.down.sql
@@ -0,0 +1,5 @@
+DROP TRIGGER IF EXISTS tag_completion_components_delete;
+DROP TRIGGER IF EXISTS tag_completion_components_update;
+DROP TRIGGER IF EXISTS tag_completion_components_insert;
+DROP INDEX IF EXISTS idx_tag_completion_component_prefix;
+DROP TABLE IF EXISTS tag_completion_components;
