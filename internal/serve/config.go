af667a7e4eecd20535aaa29672b7782e6a2395c6
fiso64 <64778819+fiso64@users.noreply.github.com>
2026-09-19 13:34:08 +0200

    feat: default-enabled independent JPEG derivative and viewer preference configuration

--- internal/serve/config.go
+++ internal/serve/config.go
@@ -95,6 +95,7 @@ type MediaConfig struct {
 	PreviewSize        int    `yaml:"preview_size"`
 	PreviewEnabled     bool   `yaml:"preview_enabled"`
 	PreviewJPEGQuality int    `yaml:"preview_jpeg_quality"`
+	LosslessJPEGTranscode bool `yaml:"lossless_jpeg_transcode"`
 }
 
 type ToolsConfig struct {
@@ -115,6 +116,7 @@ type UIConfig struct {
 	GridType                 string   `yaml:"grid_type"`
 	HiddenTags               []string `yaml:"hidden_tags"`
 	LoadFullMediaByDefault   bool     `yaml:"load_full_media_by_default"`
+	PreferLosslessFullImage bool     `yaml:"prefer_lossless_full_image"`
 	FullscreenMediaByDefault bool     `yaml:"fullscreen_media_by_default"`
 	HoverPlayVideos          bool     `yaml:"hover_play_videos"`
 	HoverPlayGIFs            bool     `yaml:"hover_play_gifs"`
@@ -172,6 +174,7 @@ func DefaultConfig(dbPath string) Config {
 			PreviewSize:        1280,
 			PreviewEnabled:     true,
 			PreviewJPEGQuality: derivativeJPEGQuality,
+			LosslessJPEGTranscode: true,
 		},
 		Tools:   ToolsConfig{FFmpegPath: "ffmpeg", FFprobePath: "ffprobe"},
 		Logging: LoggingConfig{Level: "info"},
@@ -185,6 +188,7 @@ func DefaultConfig(dbPath string) Config {
 			ViewerFitMode:          "fit_window",
 			ViewerActualSizeFitCap: true,
 			ViewerScaling:          "smooth",
+			PreferLosslessFullImage: true,
 			PaginationMode:         DefaultPaginationMode,
 			ItemsPerPage:           DefaultItemsPerPage,
 		},
