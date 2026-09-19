b9c5035f564444d4ef5cf8464014396899b37e37
fiso64 <sokfilix@gmail.com>
2026-09-19 02:07:19 +0200

    remove obvious shortcut from reference

--- frontend/src/lib/components/ShortcutsView.svelte
+++ frontend/src/lib/components/ShortcutsView.svelte
@@ -42,7 +42,6 @@
     name: 'Library actions',
     items: [
       { keys: ['(Ctrl)', 'a'], description: 'Select all files in the current view' },
-      { keys: ['⇧', '←/↑/→/↓'], description: 'Extend or shrink selection from the cursor' },
       { keys: ['d'], description: 'Download selection, or cursor file' },
       { keys: ['t'], description: 'Tag selection, or cursor file' },
       { keys: ['Alt', 'Enter'], description: 'Tag selection, or cursor file' },
